d39825d2ce32608d35848eb1c4b3e44552db04ed
bsohm2 <bsohm2@users.noreply.github.com>
2026-06-06 12:21:41 -0500

    fix: strip home row mods, simplify to plain keymap

--- keymap/default/config.h
+++ keymap/default/config.h
@@ -1,19 +1,2 @@
 #define NO_USB_STARTUP_CHECK
-
-// Disable QMK's "auto-disconnect after 10 serial errors" behavior introduced
-// post-0.14.0. The Infinity Ergodox sees occasional transient errors during
-// normal operation; the modern behavior accumulates them and stops talking
-// to the right half. The connection_errors counter caps at UINT8_MAX (255),
-// so a threshold above that effectively disables the disconnect check while
-// still keeping the 10-retry-per-transaction behavior healthy connections get.
 #define SPLIT_MAX_CONNECTION_ERRORS 65535
-
-// Enable tap-hold functionality
-#define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
-
-// Enable macros
-#define ENABLE_SEND_STRING
-
-// Reduce firmware size on 0.14.0
-#define NO_ACTION_ONESHOT
--- keymap/default/keymap.c
+++ keymap/default/keymap.c
@@ -1,56 +1,30 @@
-/* ErgoDox Infinity custom keymap layout */
+/* ErgoDox Infinity custom keymap */
 #include QMK_KEYBOARD_H
 
-// Layer definitions
 enum layers {
   QWERTY = 0,
   SYMBOLS,
   NAVIGATION,
-  NUMPAD,
   FUNCTION
 };
-
-// Macro IDs
-enum custom_keycodes {
-  MACRO_EMAIL = SAFE_RANGE,
-  MACRO_URL,
-  MACRO_CODE_SNIPPET
-};
-
-// Tap-hold definitions for home row modifiers
-#define LCTL_A LCTL_T(KC_A)
-#define LSFT_S LSFT_T(KC_S)
-#define LALT_D LALT_T(KC_D)
-#define LGUI_F LGUI_T(KC_F)
-
-#define RGUI_J RGUI_T(KC_J)
-#define RALT_K RALT_T(KC_K)
-#define RSFT_L RSFT_T(KC_L)
-#define RCTL_SCLN RCTL_T(KC_SCLN)
-
-// Layer toggle/switch keys
-#define SYM_TOG LT(SYMBOLS, KC_LBRC)
-#define NAV_TOG LT(NAVIGATION, KC_RBRC)
-#define NUM_TOG MO(NUMPAD)
-#define FN_TOG  MO(FUNCTION)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT_ergodox(
     // Left hand
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    SYM_TOG,
-    KC_LCTL, LCTL_A,  LSFT_S,  LALT_D,  LGUI_F,  KC_G,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    NAV_TOG,
-    FN_TOG,  KC_LGUI, KC_LALT, KC_PGUP, KC_PGDN,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSLS,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_PGUP, KC_PGDN,
                                                   KC_PSCR, KC_HOME,
                                                            KC_END,
                                         KC_SPC,  KC_ENT,  KC_INS,
     // Right hand
     KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    SYM_TOG, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
-             KC_H,    RGUI_J,  RALT_K,  RSFT_L,  RCTL_SCLN, KC_QUOT,
-    NAV_TOG, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    MO(SYMBOLS), KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
              KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_MUTE,
     KC_RCTL, TG(SYMBOLS),
     KC_RALT,
@@ -63,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
     KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
     KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                   KC_NO,   KC_TRNS,
                                                            KC_TRNS,
                                         KC_NO,   KC_NO,   KC_TRNS,
@@ -82,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Left hand
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-    KC_TRNS, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_NO,
+    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                   KC_TRNS, KC_TRNS,
@@ -94,27 +68,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_TRNS,
     KC_TRNS, KC_NO,   KC_BSPC, KC_DEL,  KC_NO,   KC_NO,   KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,
-    KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-
-  [NUMPAD] = LAYOUT_ergodox(
-    // Left hand
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                  KC_TRNS, KC_TRNS,
-                                                           KC_TRNS,
-                                        KC_TRNS, KC_TRNS, KC_TRNS,
-    // Right hand
-    KC_TRNS, KC_NO,   KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO,
-    KC_TRNS, KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO,
-             KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_TRNS,
-    KC_TRNS, KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_TRNS,
-             KC_TRNS, KC_P0,   KC_PDOT, KC_PENT, KC_TRNS,
     KC_TRNS, KC_TRNS,
     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
@@ -133,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Right hand
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-             KC_NO,   MACRO_EMAIL, MACRO_URL, MACRO_CODE_SNIPPET, KC_NO, KC_TRNS,
+             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS,
@@ -142,35 +95,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case MACRO_EMAIL:
-      if (record->event.pressed) {
-        SEND_STRING("developer@example.com");
-      }
-      return false;
-    case MACRO_URL:
-      if (record->event.pressed) {
-        SEND_STRING("https://github.com");
-      }
-      return false;
-    case MACRO_CODE_SNIPPET:
-      if (record->event.pressed) {
-        SEND_STRING(
-          "#include <stdio.h>" SS_DELAY(10)
-          SS_TAP(X_ENT)
-          SS_DELAY(10)
-          "int main() {" SS_DELAY(10)
-          SS_TAP(X_ENT)
-          "  printf(\"Hello, World!\");" SS_DELAY(10)
-          SS_TAP(X_ENT)
-          "  return 0;" SS_DELAY(10)
-          SS_TAP(X_ENT)
-          "}"
-        );
-      }
-      return false;
-  }
-  return true;
-}
