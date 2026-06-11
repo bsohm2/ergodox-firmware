0f23f7e52e18e07b9bf1007773894dd871c2db84
bsohm2 <bsohm2@users.noreply.github.com>
2026-06-11 14:14:23 -0500

    feat: switch to modern QMK with split watchdog, merge gaming as layer

--- keymap/default/config.h
+++ keymap/default/config.h
@@ -1,2 +1,8 @@
 #define NO_USB_STARTUP_CHECK
-#define SPLIT_MAX_CONNECTION_ERRORS 65535
+
+// Modern QMK split recovery path (enabled by QMK PR #25963, which added
+// mcu_reset support for the kiibohd bootloader): if the slave half stops
+// hearing from the master, the watchdog reboots it so it reconnects
+// instead of staying dead.
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 3000
--- keymap/default/keymap.c
+++ keymap/default/keymap.c
@@ -1,11 +1,15 @@
-/* ErgoDox Infinity custom keymap */
+/* ErgoDox Infinity custom keymap — modern QMK
+ * Layers: QWERTY base, Symbols, Navigation, Function, Gaming (toggle), Gaming mods
+ */
 #include QMK_KEYBOARD_H
 
 enum layers {
   QWERTY = 0,
   SYMBOLS,
   NAVIGATION,
-  FUNCTION
+  FUNCTION,
+  GAMING,
+  GAMING_MODS
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -15,16 +19,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSLS,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(NAVIGATION),
     KC_LCTL, KC_LGUI, KC_LALT, KC_PGUP, KC_PGDN,
                                                   KC_PSCR, KC_HOME,
                                                            KC_END,
-                                        KC_SPC,  KC_ENT,  KC_INS,
+                                        KC_SPC,  KC_ENT,  MO(FUNCTION),
     // Right hand
     KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    MO(SYMBOLS), KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    MO(SYMBOLS), KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
              KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_MUTE,
     KC_RCTL, TG(SYMBOLS),
     KC_RALT,
@@ -37,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
     KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
     KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
-    EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    EE_CLR,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                   KC_NO,   KC_TRNS,
                                                            KC_TRNS,
                                         KC_NO,   KC_NO,   KC_TRNS,
@@ -56,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Left hand
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_TRNS, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_NO,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                   KC_TRNS, KC_TRNS,
@@ -74,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [FUNCTION] = LAYOUT_ergodox(
-    // Left hand
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+    // Left hand — FN+Q toggles the Gaming layer
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT,
+    KC_TRNS, TG(GAMING), KC_NO, KC_NO,  KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -84,12 +88,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                            KC_TRNS,
                                         KC_NO,   KC_NO,   KC_TRNS,
     // Right hand
-    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+    QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+             KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_TRNS,
+    KC_TRNS, KC_NO,   KC_MPRV, KC_MNXT, KC_NO,   KC_NO,   KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS,
+    KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
+  [GAMING] = LAYOUT_ergodox(
+    // Left hand — WASD movement focused, plain mods, no tap-hold
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_RBRC,
+    KC_LALT, KC_NO,   KC_LCTL, KC_LGUI, MO(GAMING_MODS),
+                                                  KC_PSCR, KC_HOME,
+                                                           KC_END,
+                                        KC_SPC,  KC_ENT,  MO(FUNCTION),
+    // Right hand
+    KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
+             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+             KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, MO(FUNCTION),
+    KC_RCTL, TG(GAMING),
+    KC_RALT,
+    KC_RGUI, KC_SPC,  KC_ENT
+  ),
+
+  [GAMING_MODS] = LAYOUT_ergodox(
+    // Left hand — quick ability access
+    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
+    KC_TRNS, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_NO,
+    KC_TRNS, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                                  KC_NO,   KC_TRNS,
+                                                           KC_TRNS,
+                                        KC_NO,   KC_NO,   KC_TRNS,
+    // Right hand
+    KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
+             KC_L,    KC_SCLN, KC_QUOT, KC_SLSH, KC_BSLS, KC_TRNS,
+    KC_TRNS, KC_COMM, KC_DOT,  KC_EQL,  KC_MINS, KC_PLUS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_NO,   KC_TRNS,
     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
   ),
